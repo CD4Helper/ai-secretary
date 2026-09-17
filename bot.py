a1349755cfcb75686ecbc460cf68a9afcc3a20c9
Jeffrey <135301772+CD4Helper@users.noreply.github.com>
2026-09-16 19:17:10 -0700

    Lock bot to my Telegram ID

--- bot.py
+++ bot.py
@@ -5,15 +5,16 @@ from telebot import TeleBot
 # load the Telegram Bot Token from .env
 load_dotenv()
 bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+my_id = int(os.getenv("MY_TELEGRAM_ID")) # Telegram ID is number so int() is important
 print("Key loaded:", bot_token is not None)
 bot = TeleBot(bot_token)
 
 # The body part of the bot
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help'], func=lambda message: message.from_user.id == my_id)
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: message.from_user.id == my_id)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
